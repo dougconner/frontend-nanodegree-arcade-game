5771304bbc0a0ae864ca337533fbd0181945920b
Doug Conner <dconner@connerbase.com>
2015-04-20 16:36:34 -0700

    First edits to build program--no display yet

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y  = 300; // TODO: move to instance creation
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +16,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = (this.x + dt * 1) % 600;
+    this.y =
+
 }
 
 // Draw the enemy on the screen, required method for game
@@ -29,7 +34,47 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+var enemy1 = new Enemy;
+allEnemies.push(enemy1);
 
+
+/**** Player *****/
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // should just need one instance
+
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+    this.x = 300;
+    this.y  = 300; // TODO: move to instance creation
+}
+
+// Update the players's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+}
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var player = new Player;
 
 
 // This listens for key presses and sends the keys to your
