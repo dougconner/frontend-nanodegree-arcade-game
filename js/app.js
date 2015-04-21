e5023dde72db9cfc6f4668f902e9c030203ffa89
Doug Conner <dconner@connerbase.com>
2015-04-21 11:32:13 -0700

    add player movement

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 0;
-    this.y  = 300; // TODO: move to instance creation
+    this.x = -40;
+    this.y  = 0; // TODO: move to instance creation
 }
 
 // Update the enemy's position, required method for game
@@ -16,7 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = (this.x + dt * 1) % 600;
+    this.x = (this.x + dt * 0) % 600;
 
 }
 
@@ -28,14 +28,6 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var allEnemies = [];
-var enemy1 = new Enemy;
-allEnemies.push(enemy1);
 
 
 /**** Player *****/
@@ -51,8 +43,14 @@ var Player = function() {
     // The image/sprite for our player, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/char-boy.png';
-    this.x = 300;
-    this.y  = 300; // TODO: move to instance creation
+    this.startX = 202;
+    this.startY = 322;
+    this.limits = {
+      "up": -10,
+      "down": 405,
+      "left": 0,
+      "right": 40
+    };
 }
 
 // Update the players's position, required method for game
@@ -64,9 +62,32 @@ Player.prototype.update = function(dt) {
 
 }
 
-// Draw the enemy on the screen, required method for game
+// Draw the layer on the screen, required method for game
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Keyboard inputs control player motion using this method
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+        case "up":
+          if (this.y > this.limits["up"]) {
+            this.y -= 83;
+          }
+          break;
+        case "down":
+          if (this.y < this.limits["down"]) {
+            this.y += 83;
+          }
+          break;
+        case "left":
+          this.x -= 101;
+          break;
+        case "right":
+          this.x += 101;
+          break;
+    }
+    console.log(this.x + ", " + this.y);
 }
 
 
@@ -74,6 +95,17 @@ Player.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var player = new Player;
+
+// Player starts and resets to the start position
+player.x = player.startX;
+player.y = player.startY;
+console.log("player.x = " + player.x);
+console.log("player.startX = " + player.startX);
+
+var allEnemies = [];
+var enemy1 = new Enemy;
+allEnemies.push(enemy1);
+
 
 
 // This listens for key presses and sends the keys to your
